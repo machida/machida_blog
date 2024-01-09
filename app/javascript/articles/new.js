5a9bbee7cac4c46512cf18e873ee78ba45f3210c
machida <machida@fjord.jp>
2024-01-09 17:10:57 +0900

    Markdown emoji を入れた

--- app/javascript/articles/new.js
+++ app/javascript/articles/new.js
@@ -1,8 +1,20 @@
 import 'markdownit';
+import 'markdown-it-emoji';
 
 const textarea = document.getElementById('markdown-input');
 const preview = document.getElementById('markdown-preview');
-const md = markdownit();
+const md = markdownit({
+  html: true /*not default*/,
+  xhtmlOut: false,
+  breaks: true /*not default*/,
+  langPrefix: 'language-',
+  linkify: true /*not default*/,
+  typographer: true /*not default*/,
+  quotes: '“”‘’',
+  highlight: function (/*str, lang*/) { return ''; }
+});
+
+md.use(window.markdownitEmoji);
 
 textarea.addEventListener('input', function() {
   updateMarkdownPreview();
