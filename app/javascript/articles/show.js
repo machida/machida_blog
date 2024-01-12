01f210b05ea4531bdf8be7ed9bc596fda2056b90
machida <machida@fjord.jp>
2024-01-12 17:47:10 +0900

    デザインを少しだけ更新

--- app/javascript/articles/show.js
+++ app/javascript/articles/show.js
@@ -1,14 +1,25 @@
 // Markdown-itのインスタンスを作成
 const md = markdownit({
-  html: true,
+  html: true /*not default*/,
   xhtmlOut: false,
-  breaks: true,
+  breaks: true /*not default*/,
   langPrefix: 'language-',
-  linkify: true,
-  typographer: true,
+  linkify: true /*not default*/,
+  typographer: true /*not default*/,
   quotes: '“”‘’',
-  highlight: function () { return ''; }
+  highlight: function (str, lang) {
+    if (lang && Prism.languages[lang]) {
+      try {
+        return '<pre class="line-numbers language-' + lang + '"><code>' +
+               Prism.highlight(str, Prism.languages[lang], lang) +
+               '</code></pre>';
+      } catch (__) {}
+    }
+
+    return '<pre class="line-numbers language-none"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+  }
 });
+
 md.use(window.markdownitEmoji);
 
 // '.markdown2html'クラスを持つ要素を取得し、MarkdownからHTMLに変換
