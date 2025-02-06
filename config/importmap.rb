5933e56223b0ba852e922134e36df9324fbee155
machida <machida@fjord.jp>
2025-02-06 19:18:45 +0900

    Update importmap and precompile assets

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,17 +10,18 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 # フラッシュメッセージ用コントローラー
-pin "flash_controller", to: "controllers/flash_controller.js", preload: true
+pin "flash_controller", to: "/controllers/flash_controller.js", preload: true
 
 # マークダウン関連の機能
-pin "markdownSetup", to: "articles/markdownSetup.js"
-pin "imageUpload", to: "articles/imageUpload.js"
-pin "edit", to: "articles/edit.js"
-pin "show", to: "articles/show.js"
-pin "save", to: "articles/save.js"
+pin "markdownSetup", to: "/articles/markdownSetup.js"
+pin "imageUpload", to: "/articles/imageUpload.js"
+pin "edit", to: "/articles/edit.js"
+pin "show", to: "/articles/show.js"
+pin "save", to: "/articles/save.js"
+pin "new", to: "/articles/new.js"
 
 # 共有JavaScriptコンポーネント
-pin "shared/toast"
+pin "shared/toast", to: "/shared/toast.js"
 
 # 外部からのJavaScriptライブラリ
 pin "postcss-import", to: "https://cdn.jsdelivr.net/npm/postcss-import@16.0.0/+esm"
