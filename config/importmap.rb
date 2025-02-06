53aba85a615ea3d33687586fd53f110c657f6096
machida <machida@fjord.jp>
2025-02-06 19:09:49 +0900

    Update importmap and precompile assets

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,18 +1,28 @@
-# Pin npm packages by running ./bin/importmap
+# Importmap設定ファイル (config/importmap.rb)
 
+# NPMパッケージのピン
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+
+# アプリ固有のJavaScriptコントローラーファイル
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+# フラッシュメッセージ用コントローラー
+pin "flash_controller", to: "controllers/flash_controller.js", preload: true
+
+# マークダウン関連の機能
+pin "markdownSetup", to: "articles/markdownSetup.js"
+pin "imageUpload", to: "articles/imageUpload.js"
+pin "edit", to: "articles/edit.js"
+pin "show", to: "articles/show.js"
+pin "save", to: "articles/save.js"
+
+# 共有JavaScriptコンポーネント
 pin "shared/toast"
 
-pin "articles/markdownSetup"
-pin "articles/imageUpload"
-pin "articles/edit"
-pin "articles/show"
-pin "articles/save"
-
+# 外部からのJavaScriptライブラリ
 pin "postcss-import", to: "https://cdn.jsdelivr.net/npm/postcss-import@16.0.0/+esm"
 pin "markdownit", to: "https://cdn.jsdelivr.net/npm/markdown-it@14.0.0/dist/markdown-it.min.js"
 pin "markdownitEmoji", to: "https://cdn.jsdelivr.net/npm/markdown-it-emoji@3.0.0/dist/markdown-it-emoji.min.js"
